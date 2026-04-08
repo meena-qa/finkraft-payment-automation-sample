5cbc64dcc8930be12ab25634609bb6e83f037985
meena-qa <meenu1422@gmail.com>
2026-04-09 03:33:57 +0530

    Update login_test.java

--- ui_tests/login_test.java
+++ ui_tests/login_test.java
@@ -1,1 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void loginTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://example.com/login");
+
+        driver.findElement(By.id("username")).sendKeys("test_user");
+        driver.findElement(By.id("password")).sendKeys("password123");
+        driver.findElement(By.id("loginBtn")).click();
+
+        driver.quit();
+    }
+}
 
