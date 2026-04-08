b1cced3418a31ac8d23e565e12bdb109930e5bc0
meena-qa <meenu1422@gmail.com>
2026-04-09 03:36:01 +0530

    Update duplicate_invoice_checker.py

--- utils/duplicate_invoice_checker.py
+++ utils/duplicate_invoice_checker.py
@@ -1,1 +1,14 @@
+invoices = ["INV101", "INV102", "INV101", "INV103"]
+
+seen = set()
+duplicates = set()
+
+for invoice in invoices:
+    if invoice in seen:
+        duplicates.add(invoice)
+    else:
+        seen.add(invoice)
+
+print("Duplicate invoices:", duplicates)
+
 
